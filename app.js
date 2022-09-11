d5006b5e1ad6a525bb7306fcda41394de4731d84
Khoshfekr MH <97754590+KhoshfekrMH@users.noreply.github.com>
2022-09-11 20:55:43 +0430

    and now we create database named my_database and send items to human and fruits collections with mongoose

---
+++ app.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/my_database');
+
+const fruitSchema = new mongoose.Schema ({
+    name: String,
+    rating: Number,
+    review: String
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const Kiwi = new Fruit ({
+    name: "Kiwi",
+    score: 10,
+    review: "Nunc aliquam aliquam morbi interdum nec hendrerit quisque quam felis."
+});
+
+const orange = new Fruit ({
+    name: "Orange",
+    score: 4,
+    review: "Consectetur varius arcu sed eu elementum sit lorem ut eu."
+});
+
+const banana = new Fruit ({
+    name: "Banana",
+    score: 4,
+    review: "Tortor massa quam lacus congue nec lorem condimentum mi tincidunt."
+});
+
+Fruit.insertMany([Kiwi, orange, banana], function(err) {
+    if(err){
+        console.log(err);
+    } else {
+        console.log("Successfully saved all the fruits in my_database")
+    }
+});
+
+const humanSchema = new mongoose.Schema ({
+    name: String,
+    age: Number
+});
+
+const Human = mongoose.model("Human", humanSchema);
